738b792300e228e09dac4065cc5a7f137297826d
Josef21296 <jcasanovasv@hotmail.com>
2018-06-09 19:59:23 +0200

    simple xor training

--- p5.js-Projects/NeuronalNetwork/0.Intro/sketch.js
+++ p5.js-Projects/NeuronalNetwork/0.Intro/sketch.js
@@ -1,14 +1,61 @@
+const train_data = 
+{
+    "inputs": [
+        [0, 0],
+        [1, 0],
+        [0, 1],
+        [1, 1]
+    ],
+    "expect": [
+        [0],
+        [1],
+        [1],
+        [0]
+    ]
+};
+
+let tf_model;
 let fpsp;
+
+async function train(iterations = 1)
+{
+    for(let i = 0; i < iterations; ++i)
+    {
+        const response = await tf_model.fit(
+            tf.tensor2d(train_data.inputs), 
+            tf.tensor2d(train_data.expect),
+            {shuffle: true, epochs: 10}
+        );
+        //console.log('Loss[' + i + ']: ' + response.history.loss[0]);
+    }
+}
 
 function setup()
 {
     fpsp = createP("Fps: " + 0);
     createCanvas(400, 400);
+    noLoop();
+
+    // --------------------------------------------
+    
+    tf_model = tf.sequential();
+
+    tf_model.add(tf.layers.dense({units: 20, inputShape: [2], activation: 'sigmoid'}));
+    tf_model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
+
+    tf_model.compile({optimizer: tf.train.sgd(0.1), loss: 'meanSquaredError'});
+
+    train(1000).then(() => 
+    {
+        console.log('Training complete!')
+        tf_model.predict(tf.tensor2d(train_data.inputs)).print();
+        //renderFrame();
+    });
 }
 
-function draw()
+/*function renderFrame()
 {
-    fpsp.html("Fps: " + Math.floor(getFrameRate()));
+    //fpsp.html("Fps: " + Math.floor(getFrameRate()));
 
     background(0);
 
@@ -20,11 +67,24 @@ function draw()
     {
         for(let j = 0; j < rows; ++j)
         {
-            let val = random(0, 1);
-
+            let val = tf_model.predict(tf.tensor2d([[i / cols, j / rows]])).dataSync()[0];
+            //console.log(val);
             noStroke();
             fill(val * 255);
             rect(i * res, j * res, res, res);
         }
     }
-}
+
+    console.log('Frame');
+
+    train(10).then(() => 
+    {
+        renderFrame();
+    });
+}*/
+
+/*tf_model.fit(
+    tf.tensor2d(train_data.inputs),
+    tf.tensor2d(train_data.expect)
+)
+.then((res) => console.log(res.history.loss[0]));*/
